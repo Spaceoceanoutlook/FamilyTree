b57d6b858f039af8959a0c93ffbc41eab49d29fb
Spaceoceanoutlook <spaceocean@outlook.com>
2026-02-09 12:54:28 +0500

    added index.html

--- familytree/main.py
+++ familytree/main.py
@@ -1,10 +1,17 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from familytree.routes import person, tree
 
 app = FastAPI(title="Family Tree")
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(person.router)
 app.include_router(tree.router)
 

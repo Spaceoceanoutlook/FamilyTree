0cfee7741c51d63c08e10260336905c70b5162df
Spaceoceanoutlook <spaceocean@outlook.com>
2026-02-26 13:43:42 +0500

    removed versions alembic

--- familytree/repositories/person.py
+++ familytree/repositories/person.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from sqlalchemy import func, or_, select, and_
+from sqlalchemy import and_, func, or_, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from familytree.models import Person
